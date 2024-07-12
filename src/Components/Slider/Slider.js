7300331b61f62b3afb9c0aba0d691b44dd5ee12a
Artur Weclawski <weclawski.artur@gmail.com>
2024-07-12 16:09:20 +0200

    Added slider to Projects

---
+++ src/Components/Slider/Slider.js
@@ -0,0 +1,39 @@
+import { useState } from "react"
+import "./Slider.css"
+const Slider = ({ slides }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const goToPrevious = () =>{
+        const isFirstSlide = currentIndex === 0;
+        const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+        setCurrentIndex(newIndex);
+    }
+    const goToNext = () =>{
+        const isLastSlide = currentIndex === slides.length - 1;
+        const newIndex = isLastSlide ? 0 : currentIndex + 1;
+        setCurrentIndex(newIndex);
+    }
+    const goToSlide = (slideIndex) =>{
+        setCurrentIndex(slideIndex);
+    }
+
+    return(
+        <div className="sliderContainer">
+            <div className="leftArrow" onClick={goToPrevious}> &lt; </div>
+            <div className="rightArrow" onClick={goToNext}> &gt; </div>
+            <div className="slide">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            {slides[currentIndex].title}
+            </div>
+            
+            <div className="dotsContainer">
+                {slides.map((slide, slideIndex)=>(
+                    <div key = {slideIndex} className="dots" onClick={()=>goToSlide(slideIndex)}>.</div>
+                ))}
+            </div>
+        </div>
+    )
+}
+export default Slider
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,7 +1,15 @@
+import Slider from "../Components/Slider/Slider";
+
 const Projects = () =>{
+    const slides = [{url: "", title: "image-1"},
+        {url: "", title: "image-2"},
+        {url: "", title: "image-3"}
+    ]
     return(
-        <div>
-            Projects
+        <div className="Projects">
+            <div className='slider'>
+                <Slider slides={slides} />
+            </div>
         </div>
     )
 }
