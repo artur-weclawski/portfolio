4f025c1a5613baef10b61e80cb8f3af91321d802
Artur Weclawski <weclawski.artur@gmail.com>
2024-07-13 19:31:11 +0200

    Added more projects

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -5,8 +5,8 @@ import { BsGithub } from "react-icons/bs"
 import unconqueredFortressMenu from "../Components/Slider/unconquered_fortress_menu.png"
 import yeetHomePage from "../Components/Slider/yeet_home_page.png"
 const Projects = () =>{
-    const slides = [{image: unconqueredFortressMenu, title: "Unconquered Fortress", description: "A Tower Defense game created for engineering thesis. Procedurally generated levels, cloud saving, many unique enemies and defense towers. And of course, a passive tree straight from roguelike game. Game was created in Java with LibGDX framework from scratch with own graphics, physics engine and much more. Credits to @zomsik for making official repository on github."},
-        {image: yeetHomePage, title: "Yeet - web page", description: "Web page with memes created as college project. Frontend written in React.js and backend in Java Spring. For frontend was used MUI: React component library. As user you can create, edit and delete your account as well as posts and score them."}
+    const slides = [{image: unconqueredFortressMenu, title: "Unconquered Fortress", description: "A Tower Defense game created for an engineering thesis by a two-person group. It features procedurally generated levels, cloud saving, many unique enemies, and defense towers. Additionally, it includes a passive tree inspired by roguelike games. The game was developed in Java using the LibGDX framework from scratch, with original graphics, a custom physics engine, and much more."},
+        {image: yeetHomePage, title: "Yeet - web page", description: "A web page for sharing memes. You can create and edit your profile, add, view, and rate posts. You can also delete posts and your account."}
     ]
     return(
         <div className="Projects">
@@ -21,11 +21,12 @@ const Projects = () =>{
                     </div>
                     <div className="card-description">
                         <h2 style={{fontFamily:'Common Pixel', fontWeight:'100'}}>Unconquered Fortress</h2>
-                        <p> A Tower Defense game created for engineering thesis.
-                            Procedurally generated levels, cloud saving, many unique enemies and defense towers.
-                            And of course, a passive tree straight from roguelike game.
-                            Game was created in Java with LibGDX framework from scratch with own graphics, physics engine and much more.
-                            Credits to @zomsik for making official repository on github. 
+                        <p> 
+                            A Tower Defense game created for an engineering thesis. 
+                            It features procedurally generated levels, cloud saving, many unique enemies, and defense towers. 
+                            Additionally, it includes a passive tree inspired by roguelike games. 
+                            The game was developed in Java using the LibGDX framework from scratch, with original graphics, a custom physics engine, and much more. 
+                            Credits to @zomsik for creating the official repository on GitHub.
                         </p>
                         <div className="card-links">
                             <a href="https://github.com/artur-weclawski/Projekt_Game" target="_blank" rel="noreferrer" title="Original repository."><BsGithub className='BsGithub'/></a>
@@ -40,13 +41,12 @@ const Projects = () =>{
                     </div>
                     <div className="card-description">
                         <h2 style={{fontFamily:'Bungee Hairline', fontWeight:'bold', fontVariant: 'small-caps'}}>YEET</h2>
-                        <p> Web page with memes created as college project.
-                            Frontend written in React.js and backend in Java Spring.
-                            For frontend was used MUI: React component library.
-                            As user you can create, edit and delete your account as well as posts and score them.
+                        <p> 
+                            A web page for sharing memes created as a college project. 
+                            The frontend is written in React.js and the backend in Java Spring. 
+                            The frontend uses MUI, a React component library. As a user, you can create, edit, and delete your account, as well as add, edit, delete, and rate posts.
 
-                            (I think that I lost database files, there was SQL database in Xampp.)
-                            
+                            (Note: The SQL database files used in XAMPP might be lost.)
                         </p>
                         <div className="card-links">
                             <a href="https://github.com/artur-weclawski/yeet-frontend" target="_blank" rel="noreferrer" title="Frontend"><BsGithub className='BsGithub'/></a>
@@ -62,11 +62,44 @@ const Projects = () =>{
                     <div className="card-description">
                         <h2 style={{fontFamily:'Bungee Hairline', fontWeight:'bold', fontVariant: 'small-caps'}}>Memory Game</h2>
                         <p>
-                            Another college project. Game created during classes. 
+                            Another college project: a game created during classes. 
                             Your goal is to find all pairs of hidden cards.
                         </p>
                         <div className="card-links-one">
                             <a href="https://github.com/artur-weclawski/MemoryGame" target="_blank" rel="noreferrer" title="Original repository."><BsGithub className='BsGithub'/></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-image">
+                        <h2 style={{fontFamily:'Bungee Hairline', fontWeight:'bold', fontVariant: 'small-caps'}}>Wordle - Swift</h2>
+                        <p style={{fontFamily:'Bungee Hairline', fontWeight:'bold', fontSize: '5rem'}}>Wordle</p>
+                    </div>
+                    <div className="card-description">
+                        <h2 style={{fontFamily:'Bungee Hairline', fontWeight:'bold', fontVariant: 'small-caps'}}>Wordle</h2>
+                        <p>
+                            Another college project created by two-person group: a game created as a final project in Swift. 
+                            Your goal is to guess the generated word.
+                        </p>
+                        <div className="card-links-one">
+                            <a href="https://github.com/UnluckyLuki/Wordle_Project" target="_blank" rel="noreferrer" title="Original repository."><BsGithub className='BsGithub'/></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-image">
+                        <h2 style={{fontFamily:'Bungee Hairline', fontWeight:'bold', fontVariant: 'small-caps'}}>MasterAnd - Kotlin</h2>
+                        <p style={{fontFamily:'Bungee Hairline', fontWeight:'bold', fontSize: '3.5rem'}}>MasterAnd</p>
+                    </div>
+                    <div className="card-description">
+                        <h2 style={{fontFamily:'Bungee Hairline', fontWeight:'bold', fontVariant: 'small-caps'}}>MasterAnd</h2>
+                        <p>
+                            Another college project: a game created as a final project in Kotlin. 
+                            Your goal is to guess the generated color sequence.
+                            You can create local accout to check your score across different rounds.
+                        </p>
+                        <div className="card-links-one">
+                            <a href="https://github.com/artur-weclawski/MasterAnd" target="_blank" rel="noreferrer" title="Original repository."><BsGithub className='BsGithub'/></a>
                         </div>
                     </div>
                 </div>
