1d0c2ddffcc21eae14c177c74b19b36c70d455e6
artur-weclawski <weclawski.artur@gmail.com>
2025-08-17 02:05:21 +0200

    Fix animation not working on gh-pages

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -10,9 +10,19 @@ import top_background from '../top_background.png'
 import bottom_background from '../bottom_background.png'
 import Contact from "../Components/Contact/Contact";
 import { useRef, useState, useEffect } from "react";
+
+import { motion, useScroll, useTransform } from "framer-motion"; 
 const HomePage = () =>{
     const infoRef = useRef(null);
     const [size, setSize] = useState({ height: 0 });
+
+    const { scrollYProgress } = useScroll();
+
+    const topOpacity = useTransform(scrollYProgress, [0, 0.3], [1, 0]);
+    const topScale = useTransform(scrollYProgress, [0, 0.3], [1, 1.2]);
+
+    const botOpacity = useTransform(scrollYProgress, [0.5, 0.9], [0, 1]);
+    const botScale = useTransform(scrollYProgress, [0.5, 0.9], [1.2, 1]);
         
     useEffect(() => {
         const updateSize = () => {
@@ -33,9 +43,16 @@ const HomePage = () =>{
         <div className="home-page" id='home'>
             <div className="info-wrapper">
 
-                <div className="top-background" style={{ background: `url(${top_background}) no-repeat top center`, backgroundSize: 'cover' }}>
-                    {/* <img src={top_background} alt=''/> */}
-                </div>
+                <motion.div
+          className="top-background"
+          style={{
+            background: `url(${top_background}) no-repeat top center`,
+            backgroundSize: "cover",
+            opacity: topOpacity,
+            scaleY: topScale,
+            transformOrigin: "top center",
+          }}
+        />
                 <div className="info" ref={infoRef}>
                     <Neon />
                     <Description />
@@ -44,8 +61,16 @@ const HomePage = () =>{
             </div>
             <MainProjects />
             <div className="projects-wrapper">
-                <div className="bot-background" style={{ background: `url(${bottom_background}) no-repeat top center`, backgroundSize: 'cover' }}>
-                </div>
+                <motion.div
+          className="bot-background"
+          style={{
+            background: `url(${bottom_background}) no-repeat top center`,
+            backgroundSize: "cover",
+            opacity: botOpacity,
+            scaleY: botScale,
+            transformOrigin: "top center",
+          }}
+        />
                 <Projects />
             </div>
             <Contact />
